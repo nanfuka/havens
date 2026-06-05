d7cc2a9dabeeba44370bc7c10ba29e4fde71e9ab
nanfuka <kalungi2k6@gmail.com>
2026-06-05 14:17:40 +0300

    Added postgress

--- server.js
+++ server.js
@@ -1,18 +1,35 @@
+// const express = require("express");
+// const app = express();
+
+// const customerRoutes = require("./routes/customers");
+// const financeRoutes = require("./routes/finances");
+
+// app.use(express.json());
+
+// app.use("/api/customers", customerRoutes);
+// app.use("/api/finance", financeRoutes);
+
+// const PORT = 3002;
+
+// app.listen(PORT, () => {
+//   console.log(`Kirabo Havens API running on port ${PORT}`);
+// });
 const express = require("express");
 const app = express();
 
-const customerRoutes = require("./routes/customers");
-const financeRoutes = require("./routes/finances");
+const PORT = 3000;
 
+// Middleware (optional but good practice)
 app.use(express.json());
 
-app.use("/api/customers", customerRoutes);
-app.use("/api/finance", financeRoutes);
+// Home endpoint
+app.get("/", (req, res) => {
+    res.send("Hello! Your server is running 🚀");
+});
 
-const PORT = 3002;
-
+// Start server
 app.listen(PORT, () => {
-  console.log(`Kirabo Havens API running on port ${PORT}`);
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
 
 
